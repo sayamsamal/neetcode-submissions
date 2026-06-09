950d3c2eedde110ad5496223047b9c5434d77107
Sayam Samal <samal.sayam@gmail.com>
2026-06-09 20:45:02 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        vector<int> sizes;
+        string res;
+
+        for (string& s : strs) {
+            sizes.push_back(s.size());
+        }
+
+        for (int sz : sizes) {
+            res.append(to_string(sz));
+            res.push_back(',');
+        }
+
+        res.push_back('#');
+        for (string& s : strs) {
+            res.append(s);
+        }
+
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        if (s.empty()) return {};
+
+        vector<int> sizes;
+        vector<string> res;
+
+        int i = 0;
+        while (s[i] != '#') {
+            int j = i;
+            while (s[j] != ',') {
+                j++;
+            }
+            sizes.push_back(stoi(s.substr(i, j-i)));
+            i = j + 1; // s[i] == '#'
+        }
+        i++; // s[i] is at the first character to process
+
+        for (int sz : sizes) {
+            res.push_back(s.substr(i, sz));
+            i += sz;
+        }
+
+        return res;
+    }
+};
